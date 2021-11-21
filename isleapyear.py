8b9bbd10b18eeb0932fa26b0d3119ee8d215aa36
Junoburger <juno.burger@pm.me>
2021-11-21 15:53:54 +0100

    Leap year checker

--- isleapyear.py
+++ isleapyear.py
@@ -2,10 +2,20 @@ def isleapyear(year):
     if (year % 4) == 0:
         if (year % 100) == 0:
             if (year % 400) == 0:
-                print("{0} was a leap year".format(year))
+                return True
+                # print("{0} was a leap year".format(year))
             else:
-                print("{0} was not a leap year".format(year))
+                return False
+                # print("{0} was not a leap year".format(year))
         else:
-            print("{0} was a leap year".format(year))
+            return True
+            # print("{0} was a leap year".format(year))
     else:
-        print("{0} was not a leap year".format(year))
+        return False
+        # print("{0} was not a leap year".format(year))
+
+
+# mybirthyear = 2000
+
+# if isleapyear(mybirthyear):
+#     print("Year {0} was a leap year".format(mybirthyear))
